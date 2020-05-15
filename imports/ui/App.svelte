44986215f81edfd90e90e27925bacde8ded8fa98
Nandor Biro <dev@nandito.info>
2020-05-15 06:12:06 +0200

    feat: add form to add new resolution

--- imports/ui/App.svelte
+++ imports/ui/App.svelte
@@ -2,11 +2,30 @@
   import { useTracker } from 'meteor/rdb:svelte-meteor-data'
   import { Resolutions } from '../api/resolutions'
 
+  let newResolution = ''
+
+  function handleSubmit() {
+    Resolutions.insert({
+      title: newResolution,
+      createdAt: new Date()
+    })
+
+      newResolution = ''
+  }
+
   $: resolutions = useTracker(() => Resolutions.find({}).fetch())
 </script>
 
 <header>
   <h1>Yo! Yo!</h1>
+  <form on:submit|preventDefault={handleSubmit}>
+    <input
+      type="text"
+      placeholder="New resolution"
+      bind:value={newResolution}
+    >
+  </form>
+
   {#each $resolutions as resolution}
     <h2>{resolution.title}</h2>
   {/each}
